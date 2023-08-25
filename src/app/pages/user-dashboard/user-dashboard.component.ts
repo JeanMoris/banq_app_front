a2bb4997b6a5f96b7b58d1e874ce0ec4f34b92bc
Jean Moris <jeanmorisanato7@mail.ru>
2023-08-25 02:48:28 +0000

    Finalisation du routage

---
+++ src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { LightInfoInput } from 'src/app/components/light-info/light-info.component';
+
+@Component({
+  selector: 'app-user-dashboard',
+  templateUrl: './user-dashboard.component.html',
+  styleUrls: ['./user-dashboard.component.scss']
+})
+export class UserDashboardComponent implements OnInit{
+
+  accountInfoList: Array<LightInfoInput> = [];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.initializeAccountInfo()
+  }
+
+  private initializeAccountInfo(){
+    this.accountInfoList = [
+      {
+        title: 'Account balance',
+        amount: 21500,
+        infoStyle: 'bg-primary'
+      },
+      {
+        title: 'Highest transfer',
+        amount: 2100,
+        infoStyle: 'bg-warning'
+      },
+      {
+        title: 'Highest deposit',
+        amount: 32500,
+        infoStyle: 'bg-success'
+      }
+    ];
+  }
+
+
+}
